0a1c4440dd457e5daca7bb371802974de08b1bbd
CodyPluchak <pbkid21@yahoo.com>
2021-08-09 10:47:36 -0400

    cant get tests to run

--- perfectlineup.js
+++ perfectlineup.js
@@ -1,34 +1,60 @@
-const positionValidWithOneposition = ['P', 'C', '1B'. '2B', '3B', 'SS']
+const positionValidWithOneposition = ['P', 'C', '1B', '2B', '3B', 'SS']
 const positionValidWithThreeposition = ['OF']
 
 function validateLineup(lineup) {
     const salary = 0;
-    const playersATeamHas = []
-    const countOfPlayersGames = []
+    const CountOfPlayerByTeamId = []
+    const countOfPlayerByGameId = []
     const countOfPlayersPosition = []
     
-    lineup.forEach(player => {
+    lineup.forEach((player) => {
         salary = salary + player.salary
     
-        if (playersATeamHas[player.teamid] === underfined){
-            playersATeamHas[player.teamid] = 1
-        }else{
-            playersATeamHas[player.teamid] = playersATeamHas[player.teamid] + 1
+        if (CountOfPlayerByTeamId[player.teamid] === underfined){
+            CountOfPlayerByTeamId[player.teamid] = 1
+        }else {
+            CountOfPlayerByTeamId[player.teamid] = 
+            CountOfPlayerByTeamId[player.teamid] + 1
         }
-        if (countOfPlayersGames[player.gameId] === underfined){
-            countOfPlayersGames[player.gameId] = 1
-        }else{
-            countOfPlayersGames[player.gameId = countOfPlayersGames[player.gameId] + 1
+        if (countOfPlayerByGameId[player.gameId] === underfined){
+            countOfPlayerByGameId[player.gameId] = 1
+        }else {
+            countOfPlayerByGameId[player.gameId = 
+                countOfPlayerByGameId[player.gameId] + 1
             ]
         if (countOfPlayersPosition[player.position] === underfined){
             countOfPlayersPosition[player.position] = 1
-        }else{
-            countOfPlayersPosition[player.position] = countOfPlayersPosition[player.position] + 1
+        }else {
+            countOfPlayersPosition[player.position] = 
+            countOfPlayersPosition[player.position] + 1
+        }}
+    })
+
+
+        if (salary > 45000) {
+            return false
         }
 
+        for (const teamId in CountOfPlayerByTeamId) {
+            if (countOfPlayerByTeamId[teamId] > 2) {
+                return false
+            }
         }
-    };
 
+        for (const gameId in countOfPlayerByGameId) {
+            if (countOfPlayerByGameId[gameID] > 3) {
+                return false
+            }
+        }
 
-}
-module.export(validateLineup)
+        for (let x = o; x < positionValidWithThreeposition.length; x++) {
+            let position = positionValidWithThreeposition[x]
+
+            if (countOfPlayersPosition[position] !== 3) {
+                return false
+            }
+        }
+
+        return true
+    }
+module.exports(validateLineup)
